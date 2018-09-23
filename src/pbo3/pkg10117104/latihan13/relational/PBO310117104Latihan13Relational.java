7cd8476a11964a69c0382d56b4737cbe347392bf
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 19:54:47 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan13/relational/PBO310117104Latihan13Relational.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Fungsi dari Relasional
+ */
+public class PBO310117104Latihan13Relational {
+
+    public static void main(String[] args) {
+    int bil1 = 10;
+    int bil2 = 20;
+        System.out.println("a == b = " + (bil1==bil2));
+        System.out.println("a != b = " + (bil1!=bil2));
+        System.out.println("a > b = " + (bil1>bil2));
+        System.out.println("a < b = " + (bil1<bil2));
+        System.out.println("b >= a = "+(bil2>=bil1));
+        System.out.println("b <= a = " + (bil2<=bil1));
+    }
+    
+}
